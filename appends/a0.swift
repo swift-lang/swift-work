9f983df82424f96aef1be57cdfb7edda35a1cbce
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-01-09 10:47:51 -0600

    Working example- changed path for anyone to use

---
+++ appends/a0.swift
@@ -0,0 +1,16 @@
+import unix;
+import files;
+import string;
+
+append(file f, string s) {
+  string fName = filename(f);
+  string original = read(f);
+  string appended = strcat(original, s);
+  file newFile < fName >;
+  newFile = write(appended);
+}
+
+// Here is how I tested it
+file example <"example.txt">;
+example = write("line1\n");
+append(example, "line2\n");
