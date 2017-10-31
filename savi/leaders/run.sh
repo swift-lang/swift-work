6f9240ab15c9095cb9c55cc0f839e7a3f94cc355
Justin Wozniak <wozniak@mcs.anl.gov>
2017-10-31 14:55:39 -0500

    WIP

--- savi/leaders/run.sh
+++ savi/leaders/run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-if [ ${#} != 1 ]
+if [ ${#} != 2 ]
 then
-  echo "Provide the workflow number!"
+  echo "Provide <WORKFLOW NUMBER> <CLUSTER>!"
   exit 1
 fi
 W=$1
+C=$2
 
 export ADLB_DEBUG_HOSTMAP=1
-swift-t -l -t f:hosts-mcs.txt -n 8 workflow${W}.swift
+swift-t -l -t f:hosts-${C}.txt -n 8 workflow${W}.swift
