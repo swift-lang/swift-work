e32ededa756cfad52ee671b78a09e1703ab01b28
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-11-05 12:04:44 -0600

    Add numbered output- ok1 does not seem to fire first

--- mochi/py1.swift
+++ mochi/py1.swift
@@ -12,5 +12,5 @@ string code2 =
 sio = get_string_io()
 ----;
 
-python_persist(code1, "'ok'");
-python_persist(code2, "'ok'");
+trace(python_persist(code1, "'ok1'"));
+trace(python_persist(code2, "'ok2'"));
