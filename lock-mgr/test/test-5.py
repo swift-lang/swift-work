84e139e877e7688e6466a5625820d1d79e36974c
Justin M Wozniak <wozniak@mcs.anl.gov>
2019-04-16 17:00:47 -0500

    Adding

---
+++ lock-mgr/test/test-5.py
@@ -0,0 +1,59 @@
+
+# TEST 5
+# Try with SQL
+
+import random
+import time
+import sqlite3
+import _wlmpy
+
+sql_create = """
+create table records(
+rowid integer primary key,
+time timestamp,
+rank integer);
+"""
+
+conn   = None
+cursor = None
+
+def connect():
+    global conn, cursor
+    print("connect: rank=%i" % rank)
+    conn = sqlite3.connect("test.db")
+    cursor = conn.cursor()
+
+def insert():
+    global conn, rank
+    print("insert: rank=%i" % rank)
+    cmd = "insert into records (time, rank) values (%i,%i);" % \
+        (time.time(), rank)
+    conn.execute(cmd)
+    conn.commit()
+
+rank = _wlmpy.wlm_mpi_init()
+
+if rank == 0:
+    connect()
+    conn.execute(sql_create)
+    conn.commit()
+    _wlmpy.wlm_init(True)
+else:
+    _wlmpy.wlm_init(False)
+
+    # Test that the manager is ready before connecting
+    _wlmpy.wlm_acquire(0)
+    _wlmpy.wlm_release(0)
+
+    connect()
+
+    for i in range(0,5):
+        # _wlmpy.wlm_acquire(0)
+        # time.sleep(random.randint(0,100)/1000.0)
+        insert()
+        # _wlmpy.wlm_release(0)
+
+    _wlmpy.wlm_shutdown(0)
+
+_wlmpy.wlm_mpi_finalize()
+conn.close()
