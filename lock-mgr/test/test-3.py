db8c922ba93f52e59ee78bc5703104cd204682a6
Justin Wozniak <woz@anl.gov>
2019-05-20 16:29:57 -0500

    Sanity check in test

--- lock-mgr/test/test-3.py
+++ lock-mgr/test/test-3.py
@@ -1,10 +1,17 @@
 
 # TEST 3
 # Just test manager/client startup/shutdown
+# Requires at least 2 processes!
 
 import _wlmpy
 
 rank = _wlmpy.wlm_mpi_init()
+
+print(_wlmpy.wlm_mpi_size())
+
+if _wlmpy.wlm_mpi_size() < 2:
+    print("This test requires >= 2 MPI ranks!")
+    exit(1)
 
 if rank == 0:
     _wlmpy.wlm_init(True, 0)
