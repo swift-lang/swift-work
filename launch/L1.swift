ffb87699dc984a88fe9b567f0f6d1a40457fc2bb
Justin Wozniak <woz@anl.gov>
2019-05-29 15:52:29 -0500

    Add error forcing case to demonstrate swift_launcher

--- launch/L1.swift
+++ launch/L1.swift
@@ -5,5 +5,7 @@ import unix;
 v = mkdir("subdir");
 
 string args[] = [];
-string envs[] = [ "X=3", "swift_chdir=subdir" ];
+string envs[] = [ "X=3", "swift_chdir=subdir"
+                       // , "swift_launcher=ZZZ" // this will cause an error
+                        ];
 v => @par=2 launch_envs("../command.sh", args, envs);
