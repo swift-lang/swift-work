17e178ae88543c8ed0c7bbed19f65e0d4ade90f3
Justin Wozniak <woz@anl.gov>
2019-05-31 13:31:37 -0500

    Bring in Zaki method- seems to work for test-6

--- mpi4py/test_6.py
+++ mpi4py/test_6.py
@@ -1,23 +1,33 @@
 
 def get_task_comm():
     import os,sys
-    print("gtc");sys.stdout.flush()
+    print("msg1");sys.stdout.flush()
     from mpi4py import MPI
-    print("gtc2");sys.stdout.flush()
+    print("msg2");sys.stdout.flush()
     import ctypes
     task_comm_string = os.getenv("task_comm")
     print("task_comm_string: " + task_comm_string)
-    task_comm_int = int(task_comm_string)
-    MPI_Comm = ctypes.c_int
-    MPI_Comm.from_address(task_comm_int)
-    newcomm = MPI.Intracomm()
-    newcomm_ptr = MPI._addressof(newcomm)
-    comm_val = MPI_Comm.from_address(newcomm_ptr)
+    # task_comm_int = int(task_comm_string)
+    # MPI_Comm = ctypes.c_int
+    # MPI_Comm.from_address(task_comm_int)
+    # newcomm = MPI.Intracomm()
+    # newcomm_ptr = MPI._addressof(newcomm)
+    # comm_val = MPI_Comm.from_address(newcomm_ptr)
     # comm_val.value = task_comm_int
-    print("gtc3");sys.stdout.flush()
-    newcomm.barrier()
-    print("gtc4");sys.stdout.flush()
-    return newcomm
+
+    # OpenMPI mode (from Zaki)
+    comm_int = int(task_comm_string)
+    comm_pointer = ctypes.c_void_p
+    mpi4py_comm = MPI.Intracomm()
+    handle = comm_pointer.from_address(MPI._addressof(mpi4py_comm))
+    handle.value = comm_int
+    mpi4py_comm.barrier()
+
+    print("msg3");sys.stdout.flush()
+    # print("size="+str(mpi4py_comm.size()));sys.stdout.flush()
+    # mpi4py_comm.barrier()
+    print("msg4");sys.stdout.flush()
+    return mpi4py_comm
 
 def f():
     import sys
@@ -27,7 +37,7 @@ def f():
     global result
     comm = get_task_comm()
     print("comm: " + str(comm))
-    # comm.barrier()
+    comm.barrier()
     result = "XX"
 
 def get():
