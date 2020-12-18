20b6e7e7e3188e6ba9a4e7449989611d2cb914c8
Justin Wozniak <woz@anl.gov>
2020-12-18 15:30:38 -0600

    Bigger tasks

--- mpi4py/test-6.swift
+++ mpi4py/test-6.swift
@@ -1,14 +1,12 @@
 
 import python;
 
-// x1 = @par=4 python_parallel_persist("import test_6;test_6.f()",
-//                                     "repr(test_6.get())");
-
-// trace(x1);
-
 params1 = "MY PARAMETERS";
 
-x2 = @par=3 python_parallel_persist("import test_6;test_6.f('%s')" % params1,
+x1 = @par=6 python_parallel_persist("import test_6;test_6.f('INTRO')",
+                                   "repr(test_6.get())");
+
+x2 = @par=5 python_parallel_persist("import test_6;test_6.f('%s')" % params1,
                                    "repr(test_6.get())");
 
 x3 = @par=3 python_parallel_persist("import test_6;test_6.f('HELLO')",
