bf9f8991fb40d097680f56a48e2899f2601e7f2a
Justin Wozniak <woz@anl.gov>
2020-12-22 15:11:16 -0600

    Works under OpenMPI

--- mpi4py/test-3.c
+++ mpi4py/test-3.c
@@ -18,16 +18,29 @@ main()
   char code[1024];
 
   MPI_Comm comm;
-  // comm = MPI_COMM_WORLD; // MODE 1: WORKS
-  MPI_Comm_dup(MPI_COMM_WORLD, &comm); // MODE 2: WORKS NOW 2019-05-29 OpenMPI/Spack Py3
+  // comm = MPI_COMM_WORLD;
+  MPI_Comm_dup(MPI_COMM_WORLD, &comm);
   MPI_Barrier(comm);
   if (rank == 0) printf("barrier ok\n");
-  long long int i = (long long int) comm;
-  sprintf(code, "test_3.go(%lli)", i);
+  unsigned long long int comm_int;
+  comm_int = (unsigned long long int) comm;
+  sprintf(code, "test_3.go(%llu)", comm_int);
   if (rank == 0) { printf("%s\n", code); fflush(stdout); }
+
+  python_code(code);
+
+  MPI_Comm comm2;
+  MPI_Comm_split(comm, rank%2, rank, &comm2);
+  comm_int = (unsigned long long int) comm2;
+  if (rank == 0)
+    printf("C comm: %llu\n", comm_int);
+  sprintf(code, "test_3.go(%llu)", comm_int);
+  python_code(code);
+  python_code(code);
+  python_code(code);
   python_code(code);
   bool rc = python_eval("repr(42)", &result);
-  if (rank == 0) printf("rc: %i\n", rc);
+   // if (rank == 0) printf("rc: %i\n", rc);
   if (!rc)
   {
     printf("python_eval() failed!\n");
