48098f2772b23820c7e74bc745b0e410eac3349d
Justin Wozniak <woz@anl.gov>
2020-12-22 15:10:33 -0600

    Apparently works for multiple OpenMPI calls...

--- mpi4py/test_3.py
+++ mpi4py/test_3.py
@@ -1,5 +1,5 @@
 print("test_3.py ...")
-import sys
+import sys, traceback
 print("mpi4py ...") ; sys.stdout.flush()
 from mpi4py import MPI
 print("mpi4py OK ...") ; sys.stdout.flush()
@@ -8,7 +8,10 @@ import ctypes
 def go(comm_int):
     print("go(%i) ..." % comm_int)
     comm = MPI.COMM_WORLD
-    print("size: %i" % comm.Get_size())
+    world_size = comm.Get_size()
+    world_rank = comm.Get_rank()
+    print("go(): world rank: %i/%i" % (world_rank, world_size))
+    sys.stdout.flush()
     comm.barrier()
 
     # # MPICH mode:
@@ -20,20 +23,28 @@ def go(comm_int):
     # sys.stdout.flush()
     # newcomm_val = MPI_Comm.from_address(newcomm_ptr)
     # newcomm_val.value = comm_int
-    # newcomm.barrier()
-    # print("go(): barrier ok")
-    # sys.stdout.flush()
 
-    # # OpenMPI mode (from Zaki)
-    comm_pointer = ctypes.c_void_p
-    mpi4py_comm = MPI.Intracomm()
-    print("go(): comm_int: ", comm_int)
-    handle = comm_pointer.from_address(MPI._addressof(mpi4py_comm))
-    handle.value = comm_int
+    try:
+        # # OpenMPI mode (from Zaki)
+        comm_pointer = ctypes.c_void_p
+        newcomm = MPI.Intracomm()
+        print("go(): comm_int: ", comm_int)
+        sys.stdout.flush()
+        newcomm_ptr = MPI._addressof(newcomm)
+        handle = comm_pointer.from_address(newcomm_ptr)
+        handle.value = comm_int
 
-    size = comm.Get_size()
-    rank = comm.Get_rank()
-    print("go(): rank: %i/%i" % (rank, size))
-
-    mpi4py_comm.barrier()
-    print("go(): barrier ok")
+        # USER CODE:
+        new_size = newcomm.Get_size()
+        new_rank = newcomm.Get_rank()
+        if new_rank == 1:
+            print("go(): new rank: %i/%i" % (new_rank, new_size))
+        newcomm.barrier()
+        if new_rank == 1:
+            print("go(): barrier ok")
+        sys.stdout.flush()
+    except Exception as e:
+        info = sys.exc_info()
+        s = traceback.format_tb(info[2])
+        print(str(e) + ' ... \\n' + ''.join(s))
+        sys.stdout.flush()
