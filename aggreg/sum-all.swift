d572f544c73d50ccb3baaf6ba3aabb599f581ce1
Justin Wozniak <woz@anl.gov>
2021-08-31 11:39:44 -0500

    New example for Maheshwari

---
+++ aggreg/sum-all.swift
@@ -0,0 +1,33 @@
+
+/**
+   SUM ALL SWIFT
+   usage: swift-t sum-all.swift data/*.txt
+*/
+
+import io;
+import files;
+import stats;
+import sys;
+
+int S[];
+
+foreach i in [1:argc()]
+{
+  S[i] = sum_file(argp(i));
+}
+
+(int s) sum_file(string f)
+{
+  printf("input: '%s'", f);
+  string L[] = file_lines(input(f));
+  int N[];
+  foreach line, i in L
+  {
+    N[i] = string2int(line);
+  }
+  s = sum_integer(N);
+  printf("output: '%s' -> %i", f, s);
+}
+
+total = sum_integer(S);
+printf("total: %i", total);
