d485031f2340e9430022f4cd325f3bcc6ecbbaf8
Justin Wozniak <woz@anl.gov>
2022-10-05 12:02:08 -0500

    Fixes for Linux

--- tcl-ext/test.tcl
+++ tcl-ext/test.tcl
@@ -1,4 +1,9 @@
 
-# load ./libext.so
-load ./libext.dylib
+# Assemble the library shared object file name
+set libext "./libext[info sharedlibextension]"
+
+# Load the library
+load $libext
+
+# Call the function in the library:
 f
