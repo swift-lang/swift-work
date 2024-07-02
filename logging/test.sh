ee35b2b7bcf6b8368083ce1f72af9c7f627fe6e8
Justin Wozniak <woz@anl.gov>
2024-07-02 14:18:52 -0500

    Working test

--- logging/test.sh
+++ logging/test.sh
@@ -1,6 +1,11 @@
 #!/bin/zsh
 set -eu
 
-export CLASSPATH=.:log4j-core-2.23.1.jar:log4j-1.2-api-2.23.1.jar:log4j-api-2.23.1.jar
+typeset -xT CLASSPATH classpath
+classpath=( .
+            log4j2/log4j-core-2.23.1.jar
+            log4j2/log4j-1.2-api-2.23.1.jar
+            log4j2/log4j-api-2.23.1.jar
+          )
 javac t1.java
 java t1
